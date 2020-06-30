909dbcb0da08b9f1d90999c0554162b9cb6672da
alabsi91 <58077449+alabsi91@users.noreply.github.com>
2020-06-30 13:17:30 +0300

    f

---
+++ precache-manifest.c07f6e4e55aff926bf833663da3e4417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9b843b52058f61fd69e9e612139d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee5bef08a11d3a0a337c",
+    "url": "/static/css/main.9601bc1a.chunk.css"
+  },
+  {
+    "revision": "c8beff09c837d4d04c02",
+    "url": "/static/js/2.5d3a3c73.chunk.js"
+  },
+  {
+    "revision": "03f63cf1bbbd491c6ac26011b416fd46",
+    "url": "/static/js/2.5d3a3c73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5bef08a11d3a0a337c",
+    "url": "/static/js/main.99c040a8.chunk.js"
+  },
+  {
+    "revision": "a08d0ccbf8e6e02ec7f7",
+    "url": "/static/js/runtime-main.38f610d4.js"
+  }
+]);
